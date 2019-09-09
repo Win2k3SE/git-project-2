3c85e17af0302566bf2084d62f614e2bee2e4124
zmv <zmv@gmail.com>
2019-09-09 23:16:52 +0400

    added fName and lName fields

--- src/com/gittest/Main.java
+++ src/com/gittest/Main.java
@@ -1,6 +1,9 @@
 package com.gittest;
 
 public class Main {
+	
+	private static final String fName = "Jeonghan";
+	private static final String lName = "Yoon";
 
 	public static void main(String[] args) {
 		
