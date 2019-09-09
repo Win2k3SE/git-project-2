860178dad0f06edde4c7083f5fdf4499e66c6cf9
zmv <zmv@gmail.com>
2019-09-09 23:36:03 +0400

    added sayTheName method

--- src/com/gittest/Main.java
+++ src/com/gittest/Main.java
@@ -5,17 +5,19 @@ public class Main {
 	private static final String fName = "Jeonghan";
 	private static final String lName = "Yoon";
 
-	
 	public static String getFname() {
 		return fName;
 	}
-
-
 	public static String getLname() {
 		return lName;
 	}
-
-
+	public static String generateTheName() {
+		return "My name is " + getLname() + " " + getFname() + "! Nice to meet you!";
+	}
+	public static void sayTheName() {
+		System.out.println(generateTheName());
+	}
+	
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!");
