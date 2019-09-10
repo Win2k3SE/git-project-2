986a0c47616054cade81eb0e4c2f55ca2bbf1480
zmv <zmv@gmail.com>
2019-09-10 10:47:50 +0400

    remove all comments

--- src/com/gittest/Main.java
+++ src/com/gittest/Main.java
@@ -4,23 +4,13 @@ import com.gittest.domain.Person;
 import com.gittest.services.NameSayer;
 
 public class Main {
-	// я хуею и ниче не понимаю   !!!
-	public static void hui() {
-		System.out.println("hui");
-	}
-	// ебись оно конем!!!
+
 	public static void main(String[] args) {
 		
 		Person yjh = new Person("Jeonghan", "Yoon");
 		
 		NameSayer.sayTheName(yjh);
 
-		// first modif on branch crazy idea
-		// 2nd commit on crazy idea
-
-		// modify inside main on master
 	}
 	
-	// modifying the line below main() 
-	// this shouldn't result in a conflict
 }
